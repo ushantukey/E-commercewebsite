0ec23e90b56eff490d9794bb8802f1ada92c3983
ushantukey <ushantukey16@gmail.com>
2025-08-17 17:12:28 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-pink-800 font-bold text-center'>working</h1>
+    </div>
+  )
+}
+
+export default App
