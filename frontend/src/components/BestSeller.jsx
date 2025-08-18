a2a00738d20a142be58c2c91bc6abcda355e9e64
ushantukey <ushantukey16@gmail.com>
2025-08-18 21:00:37 +0530

    Done with footer, bestseller components

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,35 +3,40 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem'
 
-const LatestCollection = () => {
+const BestSeller = () => {
 
     const { products } = useContext(ShopContext)
-    const [latestProducts, setLatestProducts] = useState([]);
+    const [bestSeller, setBestSeller] = useState([])
 
     useEffect(() => {
-        setLatestProducts(products.slice(0, 10))
-    }, [])
+        const bestProduct = products.filter((item) => item.bestseller)
+        setBestSeller(bestProduct.slice(0, 5))
+    }, [])  
 
     return (
         <div className='my-10'>
             <div className='text-center py-8 text-3xl'>
-                <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+                <Title text1={'BEST'} text2={'SELLERS'} />
                 <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
                     Discover the latest trends in fashion, electronics, and home essentials. Shop your favorite brands at unbeatable prices and enjoy fast, secure delivery right to your doorstep.
-
                 </p>
             </div>
-            {/* Rendring products */}
+            {/* Rendering products */}
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
-                    latestProducts.map((item,index)=>(
-                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                    bestSeller.map((item, index) => (
+                        <ProductItem 
+                            key={index} 
+                            id={item._id} 
+                            image={item.image} 
+                            name={item.name} 
+                            price={item.price} 
+                        />
                     ))
                 }
             </div>
-
         </div>
     )
 }
 
-export default LatestCollection
+export default BestSeller
