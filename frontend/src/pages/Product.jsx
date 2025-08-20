4257375b3a1591e2cef189d6fe51e0523abc6942
ushantukey <ushantukey16@gmail.com>
2025-08-20 20:51:32 +0530

    create product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
-  return (
-    <div>
-      
+
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('')
+  const [size, setSize] = useState('')
+
+  const fetchProductData = async () => {
+
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item)
+        setImage(item.image[0])
+        return null;
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId])
+
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        {/* Product Image */}
+
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img src={image} className='w-full h-auto' alt="" />
+          </div>
+        </div>
+        {/* Product Info */}
+
+        <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className='w-3 5' />
+            <img src={assets.star_icon} alt="" className='w-3 5' />
+            <img src={assets.star_icon} alt="" className='w-3 5' />
+            <img src={assets.star_icon} alt="" className='w-3 5' />
+            <img src={assets.star_dull_icon} alt="" className='w-3 5' />
+            <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className="flex gap-2 flex-wrap">
+              {productData.sizes.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => setSize(item)}
+                  className={`px-4 py-2 rounded-full border transition-all duration-200 
+        ${item === size
+                      ? 'border-orange-500 bg-orange-100 text-orange-600 font-medium'
+                      : 'border-gray-300 bg-gray-100 hover:border-gray-500 hover:bg-gray-200'}`}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+
+          </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original</p>
+            <p>Cash on delivery</p>
+            <p>Easy return</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Description and review section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+        </div>
+
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>This premium cotton t-shirt is crafted for all-day comfort and style. Featuring a modern fit, breathable fabric, and durable stitching, it’s perfect for casual wear or layering. Designed to give you maximum comfort while keeping your style fresh.
+          </p>
+          <p>This premium cotton t-shirt is crafted for all-day comfort and style. Featuring a modern fit, breathable fabric, and durable stitching, it’s perfect for casual wear or layering. Designed to give you maximum comfort while keeping your style fresh.
+          </p>
+        </div>
+      </div>
+
+      {/* Display related products */}
+
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
